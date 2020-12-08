4ccc7696cc7a0811d0d0ca6115e0c2f93122fb93
Md Nasirul Haque <talk2lurisan@gmail.com>
2020-12-08 18:48:35 +0530

    made basic changes for calculation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,87 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  inputData: any = '';
+  newMonth: any = '';
+  monthlyTotal: any = '';
+
   ngOnInit(): void {
+  }
+
+  Generate() {
+    console.log(this.inputData);
+    this.calculateFunct(this.inputData)
+  }
+
+  calculateFunct(fileData: any) {
+    // const fse = require('fs-extra')
+
+    // let fileName = (process.argv[2]) ? process.argv[2] : 'data'
+    // let fileData = fse.readFileSync(fileName + '.txt', 'utf8')
+
+
+    let fileDataArray = fileData.split('\n')
+    let monthlyTotal = []
+    let totalsum = 0
+    let yeartotal = 0
+    let month = parseInt(fileData.split('\n')[0].split(':')[0].split('/')[1])
+    let year = parseInt(fileData.split('\n')[0].split(':')[0].split('/')[2])
+
+    for (let i of fileDataArray) {
+      let sum = 0
+      if (i.match('/') < 2) {
+        yeartotal = yeartotal + totalsum
+        totalsum = 0
+        monthlyTotal.push(i)
+        continue
+      }
+      if (i.match(':') > 1) {
+        sum = parseFloat(i.split(' : ')[2].replace(',', ''))
+        totalsum = totalsum + sum
+        monthlyTotal.push(i)
+        continue
+      }
+      // console.log(i)
+      let loc = i.split(' : ')[1]
+      if ((loc.split(' + ')).length > 1 && loc.split(' + ')[0] != '') {
+        for (let j of loc.split(' + '))
+          sum = sum + parseFloat(j.replace(',', ''))
+        let formated = (sum + '').replace('.00', '')
+        monthlyTotal.push(i + ' : ' + formated)
+        totalsum = totalsum + sum
+      } else if (loc.split(' + ')[0] != '') {
+        monthlyTotal.push(i + ' : ' + loc.split(' + ')[0])
+        totalsum = totalsum + parseFloat(loc.split(' + ')[0].replace(',', ''))
+      } else
+        monthlyTotal.push(i)
+
+      yeartotal = yeartotal + totalsum
+    }
+
+    let formated = (totalsum + '').replace('.00', '')
+    monthlyTotal.push('\n------------------------ ' + formated + ' : ' + formated + ' -------------------------\n')
+
+    let newMonth = []
+    let end = 31
+    year = (month == 12) ? year + 1 : year
+    month = (month < 12) ? month + 1 : 1
+
+    if ([1, 3, 5, 7, 8, 10, 12].includes(month))
+      end = 32
+    else if (month == 2)
+      end = 29
+    for (let i = 1; i < end; i++)
+      newMonth.push(i + '/' + month + '/' + year + ' : ')
+
+    newMonth.push('\n------------------------  :  -------------------------')
+
+    console.log(totalsum)
+    console.log(newMonth.join('\n'));
+    console.log(monthlyTotal.join('\n'));
+    this.newMonth = newMonth.join('\n');
+    this.monthlyTotal = monthlyTotal.join('\n');
+    // fse.writeFileSync('m.total.txt', monthlyTotal.join('\n'))
+    // fse.writeFileSync('nm.txt', newMonth.join('\n'))
   }
 
 }
